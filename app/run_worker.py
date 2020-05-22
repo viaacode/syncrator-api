a1373a8114e01db6dc47357bf3953b77af993513
Walter Schreppers <schreppers@gmail.com>
2020-05-22 16:37:53 +0200

    final cleanup

--- app/run_worker.py
+++ app/run_worker.py
@@ -23,19 +23,13 @@ class RunWorker(threading.Thread):
             'api_job_id': self.api_job_id,
             'job_params': self.job_params
         })
-       
+ 
+        # syncrator pod will update the api_job.status first with "running"
+        # later on with "completed" or "failed" etc.
         result = oc_create_syncrator_pod( self.job_params )
-        
-        # TODO: maybe add some checks in result if something fails 
-        # then we set ApiJob.status = failed
-        # we can do this conveniently using passed api_job_id here
 
-        # problem, we have no valid db session here!
-        #api_job = ApiJob.query.filter_by(id=api_job_id).first()
-        #api_job.status = "started"
-        #db.session.commit()
-
-        self.logger.info( 'RunWorker for api_job_id={} succesfully started syncrator'.format(self.api_job_id), data=result)
+        self.logger.info( 'RunWorker for api_job_id={} succesfully started syncrator'.format(
+            self.api_job_id), data=result)
 
 
        
